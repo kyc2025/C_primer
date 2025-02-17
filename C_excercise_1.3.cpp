9632eea389037a6dbc5bcb3ad7851eb4c8e25b99
kaiyuchen <great5361823@gmail.com>
2025-02-17 23:18:01 +0800

    upload excercise1.3

---
+++ C_excercise_1.3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main()
+{
+	std::cout << "Hello, World" << std::endl; // ex1.3
+	int v1=0, v2=0;
+	std::cin>>v1>>v2;
+	std::cout << "the multiplication of " <<v1<< " and "<<v2 <<" is "<<v1*v2<< std::endl; // ex1.4
+	
+	std::cin>>v1>>v2;
+	std::cout << "v1="<<v1<<", "<<"v2="<<v2<<", "<<"v1*v2="<<v1*v2<<std::endl;// exp1.5
+	
+	return 0;
+}
